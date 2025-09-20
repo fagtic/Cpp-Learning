2da77df3b35e19bcd1330e9c1ccd16872a939eb9
fagtic <68462729+fagtic@users.noreply.github.com>
2025-09-20 18:58:28 +0300

    Update Triangle_type.cpp
    added spaces for better readability and replaced << endl with "\n"

--- Triangle_type.cpp
+++ Triangle_type.cpp
@@ -4,18 +4,19 @@ using namespace std;
 int main() {
 
     int a,b,c,sides;
-    cout<<"Enter the a "<<endl;
-    cin>>a;
-    cout<<" Enter the b "<<endl;
-    cin>>b;
-    cout<<"Enter the c"<<endl;
-    cin>>c;
+    cout << "Enter the a \n";
+    cin >> a;
+    cout << " Enter the b \n";
+    cin >> b;
+    cout << "Enter the c \n";
+    cin >> c;
 
-    if (a*a==b*b + c*c || b*b==c*c+a*a || c*c==a*a+b*b){
-        cout<<" Right angle Triangle."<<endl;
+    if (a*a == b*b + c*c || b*b == c*c + a*a || c*c == a*a + b*b){
+        cout <<" Right angle Triangle. \n";
     }
     else {
-        cout<<" Not a right angle triangle"<<endl;
+        cout <<" Not a right angle triangle \n";
     }
     return 0;
+
 }
